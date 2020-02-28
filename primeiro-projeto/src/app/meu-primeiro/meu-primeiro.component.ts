5bdb82f58f42293b94d5ff296ec967dab81bf7fe
sonsyrock <42223934+sonsyrock@users.noreply.github.com>
2020-02-28 00:31:34 -0300

    Terminado Aula 3 - Ir para Aula 4

---
+++ primeiro-projeto/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+@Component({
+
+    selector: 'meu-primeiro-component',
+    template: `
+     <p>Meu primeiro component com Angular2</p>
+     `
+    
+})
+export class MeuPrimeiroComponent{
+
+
+};
