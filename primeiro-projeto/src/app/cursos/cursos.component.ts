a9d4e53e737a33dfc3e26e1b5669200e8e4d3f67
sonsyrock <42223934+sonsyrock@users.noreply.github.com>
2020-03-03 19:22:47 -0300

    Aula 6 - Terminado

--- primeiro-projeto/src/app/cursos/cursos.component.ts
+++ primeiro-projeto/src/app/cursos/cursos.component.ts
@@ -7,7 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CursosComponent implements OnInit {
 
-  constructor() { }
+  nomePortal: any;
+
+  cursos: string[] = ['Java', 'Ext JS', 'Angular']
+
+  constructor() { 
+
+    this.nomePortal = 'http://loiane.training';
+
+  }
 
   ngOnInit(): void {
   }
