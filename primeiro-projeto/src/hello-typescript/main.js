63017569699d82aa3ba2f95aa81ef015e8a17701
sonsyrock <42223934+sonsyrock@users.noreply.github.com>
2020-03-03 18:59:33 -0300

    Aulas 4 e 5 Terminadas - Commit Executado

---
+++ primeiro-projeto/src/hello-typescript/main.js
@@ -0,0 +1,20 @@
+var minhaVar = 'minha variavel';
+function minhaFunc(x, y) {
+    return x + y;
+}
+var num = 2;
+var PI = 3.14;
+var numeros = [1, 2, 3];
+numeros.map(function (valor) {
+    return valor * 2;
+});
+//Arrow Function ES2015
+numeros.map(function (valor) { return valor * 2; });
+var Matematica = /** @class */ (function () {
+    function Matematica() {
+    }
+    Matematica.prototype.soma = function (x, y) {
+        return x + y;
+    };
+    return Matematica;
+}());
